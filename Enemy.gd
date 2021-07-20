f3fa6d28f4b4ceb1871ffad5c04526983ba6ef5b
Shylexx <alexanderdauncey@gmail.com>
2021-07-20 18:46:31 +0100

    Initial commit

---
+++ Enemy.gd
@@ -0,0 +1,53 @@
+extends Area2D
+
+export var speed : int = 100
+export var moveDist : int = 100
+
+onready var startX : int = position.x
+onready var targetX : int = position.x + moveDist
+
+func _process(delta):
+	
+	position.x= move_to(position.x, targetX, speed * delta)
+	
+	if position.x == targetX:
+		if targetX == startX:
+			targetX = position.x + moveDist
+		else:
+			targetX = startX
+
+func move_to(current, to, step):
+	
+	var new = current
+	
+	if new < to:
+		new += step
+		
+		if new > to:
+			new = to
+	else:
+		new -= step
+		
+		if new < to:
+			new = to
+	
+	return new
+# Declare member variables here. Examples:
+# var a = 2
+# var b = "text"
+
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	pass # Replace with function body.
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+#func _process(delta):
+#	pass
+
+
+func _on_Enemy_body_entered(body):
+	if body.name == "Player":
+		body.die()
+	
---
+++ README.md
@@ -0,0 +1,2 @@
+# FirstGodotProject
+ Very Basic 2D Platformer Created for a Zenva Course ("Bitesized Godot Development")
